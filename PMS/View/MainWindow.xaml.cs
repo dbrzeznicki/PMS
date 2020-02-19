108a602894ce6692a776218fa6e3eea1a4b36ecf
dbrzeznicki <d.brzeznicki@wp.pl>
2020-02-19 13:38:00 +0100

    First commit (GUI)

---
+++ PMS/View/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PMS
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton(object sender, RoutedEventArgs e)
+        {
+            Login();
+        }
+
+        private void RegisterButton(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Login();
+            } 
+            else if (e.Key == Key.Escape)
+            {
+                Close();
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Login()
+        {
+
+            //sprawdzić kto zalogowany potem
+            Admin adminWindow = new Admin();
+            App.Current.MainWindow.Close();
+            App.Current.MainWindow = adminWindow;
+            adminWindow.Show();
+        }
+    }
+}
---
+++ PMS/ViewModel/SubItemMenu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PMS.ViewModel
+{
+    public class SubItemMenu
+    {
+        public string Name { get; set; }
+        public UserControl UserControl { get; set; }
+
+        public SubItemMenu(string name, UserControl userControl = null)
+        {
+            Name = name;
+            UserControl = userControl;
+        }
+
+    }
+}
+
